7e8bcae7b25590624d7d16db236491f2e725f5e1
Velislava Dimitrova <vili.alias@gmail.com>
2022-03-16 18:39:54 +0200

    commit

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,16 +1,42 @@
-
-
 import { test, expect } from '@playwright/test';
+import { Label } from 'allure-js-commons';
+import { allure } from 'allure-playwright'
 
 test('basic test', async ({ page }) => {
-  await page.goto('');
+    allure.severity('critical')
+    allure.story('Story 1')
+    await page.goto('');
 
-  console.log(page.frames());
+    // console.log(page.frames());
 
-  const firstName = page.locator('[src="img/logos/Browsers.png"]');
-  await expect(firstName).toBeVisible();
+    const firstName = page.locator('[src="img/logos/Browsers.png"]');
+    await expect(firstName).toBeVisible();
+    await expect('dfdf').toBe('f')
+});
+
+test('basic test 1', async ({ page }) => {
+    allure.severity('normal')
+    allure.story('Story 2')
+    let label:Label = {name:'label1',value:'labelValue'}
+    allure.label(label)
+    allure.id('id1')
+    await page.goto('');
+
+    // console.log(page.frames());
+
+    const firstName = page.locator('[src="img/logos/Browsers.png"]');
+    await expect(firstName).toBeVisible();
 });
 
 
+test('basic test 2', async ({ page }) => {
+    allure.severity('high')
+    allure.story('Story 3')
+    await page.goto('');
 
+    // console.log(page.frames());
 
+    const firstName = page.locator('[src="img/logos/Browsers.png1"]');
+    firstName.click();
+    
+});
